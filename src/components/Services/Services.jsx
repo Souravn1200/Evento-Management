38b2a3f9bc8b9ee3018ec60e6dc47d28b676534d
Souravn1200 <souravn1200@gmail.com>
2023-10-08 06:31:38 +0600

    Made Nav, Banner and Loding Service Data on Home Page

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
 
-const Services = () => {
+
+const Services = ({eachData}) => {
+  
+    const {id, name, image, price, description} = eachData
+    
     return (
         <div>
             <h2>
-                this is services
+            <div className=" bg-base-100 shadow-xl">
+  <figure><img className="h-[220px] w-full" src={image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{name}</h2>
+    <p>{description}</p>
+    <div className="card-actions flex mt-3">
+        <p className="font-bold">{price}</p>
+      <button className="btn btn-primary">Buy now</button>
+    </div>
+  </div>
+</div>
+        
             </h2>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
+import { useLoaderData } from "react-router-dom";
+import Banner from "../components/Navbar/Banner/Banner";
 import Services from "../components/Services/Services";
+// import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+const data = useLoaderData();
+
+    // const [data, setData] =  useState([]);
+
+    // useEffect(() => {
+    //     fetch('/public/Events.json')
+    //     .then(res => res.json())
+    //     .then(result =>setData(result))
+    //     .catch(err => console.log(err));
+    // },[])
+
     return (
         <div>
-            <h2>This is Home</h2>
-            <Services></Services>
+            <Banner></Banner>
+
+
+            <div className="grid lg:grid-cols-3 gap-6 mt-6">
+                
+            {data.map(eachData => <Services key={eachData.id} eachData={eachData}></Services>)}
+            </div>
+
+            
            
         </div>
     );
